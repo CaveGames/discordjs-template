46c3ef4d6b4d489d059ce1910e79bcec7b55bb57
Peace <Debitor.TooLP@gmx.de>
2021-11-29 12:18:14 +0100

    added database usage example

--- src/commands/test/TestCommand.js
+++ src/commands/test/TestCommand.js
@@ -1,8 +1,17 @@
+const { Test } = require('../../database').models;
+
 module.exports = {
 	command: 'test',
 	onlyAdmin: false,
 
 	async run(client, message, args) {
-		message.channel.send('Test command works');
+
+		const test = await Test.findOne({
+			where: {
+				message: 'test',
+			},
+		});
+
+		message.channel.send('Hello World!');
 	},
 };
